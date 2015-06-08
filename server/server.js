dab5c341f8a69b2f3f4dae7fbd37c4d0bcc01abc
David Neubauer <davidneub@gmail.com>
2015-06-08 15:55:07 +0200

    socket listeners

--- server/server.js
+++ server/server.js
@@ -2,19 +2,40 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('Hello!');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     socket.emit('serverReady');
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
         console.log('user disconnected');
     });
 
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+    socket.on('newMessage', function(msg) {
+        console.log('new message: ' + msg);
+        socket.emit('message', { message: 'world', user: 'test' });
+    });
+
+    socket.on('newUser', function(msg) {
+        console.log('new user: ' + msg);
+    });
+
+    socket.on('newRoom', function(msg) {
+        console.log('new room: ' + msg);
+    });
+
+    socket.on('removeRoom', function(msg) {
+        console.log('remove room: ' + msg);
+    });
+
+    socket.on('joinRoom', function(msg) {
+        console.log('join room: ' + msg);
+    });
+
+    socket.on('leaveRoom', function(msg) {
+        console.log('leave room: ' + msg);
     });
 });
 
