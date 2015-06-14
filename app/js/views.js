bbd9e07aa1e6362581137f21e8abdc7719fee8a0
David Neubauer <davidneub@gmail.com>
2015-06-14 14:02:55 +0200

    removed obsolet usersCollection in chatView

--- app/js/views.js
+++ app/js/views.js
@@ -31,8 +31,6 @@ class ChatView extends Backbone.View {
 
     constructor() {
         super(...arguments);
-
-        this.usersCollection = UserCollection;
     }
 
     // Events Property
@@ -61,10 +59,7 @@ class ChatView extends Backbone.View {
     }
 
     render() {
-
-        this.$el.html(this.template({
-            users: this.usersCollection.toJSON()
-        }));
+        this.$el.html(this.template);
 
         var messageView = new MessageView();
         this.$el.find('.view-chat__messages').prepend(messageView.render().$el);
